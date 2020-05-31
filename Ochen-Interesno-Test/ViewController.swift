5142f082d905a33484941017f985766b7efcc105
Artem Rylov <tema2707@icloud.com>
2020-05-31 21:45:37 +0700

    first commit

---
+++ Ochen-Interesno-Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ochen-Interesno-Test
+//
+//  Created by art-off on 31.05.2020.
+//  Copyright © 2020 art-off. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
