4e1e1725293d9ddf1f6c5f95201980189b497fe3
Artem Rylov <tema2707@icloud.com>
2020-05-31 21:56:23 +0700

    Добавил enum API и описал модели ответа API

---
+++ Ochen-Interesno-Test/Model/API.swift
@@ -0,0 +1,16 @@
+//
+//  API.swift
+//  Ochen-Interesno-Test
+//
+//  Created by art-off on 31.05.2020.
+//  Copyright © 2020 art-off. All rights reserved.
+//
+
+import Foundation
+
+enum API {
+    
+    static let addres = "https://serpapi.com/search.json"
+    static let key = "fbcca12b9dc69bdd30c3bffa8d7e41a369bcdaa2b0e52b55fd7b70fc9b06d7b1"
+    
+}
---
+++ Ochen-Interesno-Test/Model/ImagesResponse.swift
@@ -0,0 +1,30 @@
+//
+//  ImagesResponse.swift
+//  Ochen-Interesno-Test
+//
+//  Created by art-off on 31.05.2020.
+//  Copyright © 2020 art-off. All rights reserved.
+//
+
+import Foundation
+
+struct ImagesResponse: Codable {
+    
+    let imagesResults: [ImagesResults]
+    
+    enum CodingKeys: String, CodingKey {
+        case imagesResults = "images_results"
+    }
+    
+}
+
+struct ImagesResults: Codable {
+    
+    let position: Int
+    let thumbnail: String
+    let original: String
+    let source: String
+    let title: String
+    let link: String
+    
+}
