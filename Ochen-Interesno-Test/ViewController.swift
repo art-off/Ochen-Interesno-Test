29c82bcc28c63799723880e94b5051334fd00404
Artem Rylov <tema2707@icloud.com>
2020-06-01 20:45:51 +0700

    Удалил Search Controller и добавил Text Field

--- Ochen-Interesno-Test/ViewController.swift
+++ Ochen-Interesno-Test/ViewController.swift
@@ -12,70 +12,47 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     
-    
-    // MARK: - Для Search Controller
-    private let searchController = UISearchController(searchResultsController: nil)
-    
-    private var searchBarIsEmpty: Bool {
-        guard let text = searchController.searchBar.text else { return false }
-        return text.isEmpty
-    }
-    
-    private var isFiltering: Bool {
-        return searchController.isActive && !searchBarIsEmpty
-    }
+    @IBOutlet weak var searchTextField: UITextField!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // закрытие клавиатуры по нажатию вне ее
+        addTapGestureToHideKeyboard()
+        
         navigationItem.title = "Поиск изображений"
         
-        setupSearchController()
-        
-        
-        ApiManager.downloadJsonImages(withSearchText: "mojong") { imagesResults in
-            
-            let base64 = imagesResults![5].thumbnail!
-            
-            let correctBase64 = String(base64.suffix(from: base64.index(base64.firstIndex(of: ",")!, offsetBy: 1)))
+        searchTextField.delegate = self
+    }
 
+}
+
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        guard let searchText = textField.text else { return false }
+        guard !searchText.isEmpty else { return false }
+        
+        
+        ApiManager.downloadJsonImages(withSearchText: searchText) { imagesResults in
+        
+            let base64 = imagesResults![5].thumbnail!
+        
+            let correctBase64 = String(base64.suffix(from: base64.index(base64.firstIndex(of: ",")!, offsetBy: 1)))
+        
             let imageData = Data(base64Encoded: correctBase64)!
             let image = UIImage(data: imageData)
-
-//            for i in 0..<100 {
-//                print(imagesResults![i])
-//            }
-            
+        
             print(imagesResults![55])
             
             DispatchQueue.main.async {
                 self.imageView.image = image
             }
         }
-    }
-    
-    private func setupSearchController() {
-        searchController.searchResultsUpdater = self
-        searchController.obscuresBackgroundDuringPresentation = false
-        searchController.searchBar.placeholder = "Введите название изображения"
         
-        navigationItem.searchController = searchController
-        definesPresentationContext = true
-        
-        // чтобы строка поиска всегда отображался
-        navigationItem.hidesSearchBarWhenScrolling = false
-    }
-
-
-}
-
-extension ViewController: UISearchResultsUpdating {
-    
-    func updateSearchResults(for searchController: UISearchController) {
-        guard let searchText = searchController.searchBar.text else { return }
-        
-        print("searchd")
+        view.endEditing(true)
+        return true
     }
     
 }
